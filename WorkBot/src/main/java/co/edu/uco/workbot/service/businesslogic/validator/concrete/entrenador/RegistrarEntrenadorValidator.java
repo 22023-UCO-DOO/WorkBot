a320377bc8886b6afd28c5b6263a80995f675051
Seletex <sarr1998@hotmail.com>
2023-11-05 17:18:20 -0500

    Cambios para el proyecto

--- WorkBot/src/main/java/co/edu/uco/workbot/service/businesslogic/validator/concrete/entrenador/RegistrarEntrenadorValidator.java
+++ WorkBot/src/main/java/co/edu/uco/workbot/service/businesslogic/validator/concrete/entrenador/RegistrarEntrenadorValidator.java
@@ -1,5 +1,25 @@
 package co.edu.uco.workbot.service.businesslogic.validator.concrete.entrenador;
 
-public class RegistrarEntrenadorValidator {
+import co.edu.uco.workbot.service.businesslogic.validator.Validator;
+import co.edu.uco.workbot.service.domain.calendario.CalendarioDomain;
+import co.edu.uco.workbot.service.domain.calendario.rule.FechaCalendarioValidationRule;
+import co.edu.uco.workbot.service.domain.calendario.rule.IdCalendarioValidationRule;
 
+public class RegistrarEntrenadorValidator implements Validator<CalendarioDomain>{
+
+	private static final Validator<CalendarioDomain> instancia = new RegistrarEntrenadorValidator();
+	private RegistrarEntrenadorValidator() {
+		
+	}
+	
+	public static final void ejecutarValidacion(final CalendarioDomain dato) {
+		instancia.execute(dato);
+	}
+	
+	@Override
+	public void execute(final CalendarioDomain data) {
+		IdCalendarioValidationRule.ejecutarValidacion(data.getId());
+		FechaCalendarioValidationRule.ejecutarValidacion(data.getFecha());
+		
+	}
 }
